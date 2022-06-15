e038cf433396016cc92a6495f7524f0d9d1c404a
Aure <swishswish123@gmail.com>
2022-06-15 12:35:27 +0100

    fix, clean, comm: combined utils, minor fixes and changes
    clean:
    - combined all utils functions to one
    - deleted some unused functions
    
    fix:
    - 2D + 3D noise of automatic now different
    
    comm:
    - commented on all utils functions

--- endosim/notebooks/endosimutilities/end_utils.py
+++ endosim/notebooks/endosimutilities/end_utils.py
@@ -27,6 +27,7 @@ def create_transform(array_of_parameters):
     transform = mu.construct_rigid_transformation(rotation, translation)
     return transform
 
+
 def multiply_points_by_matrix(matrix_4x4, matrix_of_points, do_transpose):
     """
     Multiplies all points by the same matrix.
@@ -49,24 +50,63 @@ def multiply_points_by_matrix(matrix_4x4, matrix_of_points, do_transpose):
 
 
 def multiply_point_by_matrix(transform, point_as_array):
+    """
+    multiply_point_by_matrix(transform, point_as_array)
+    multiplies a 3D point by a 4X4 transformation
+
+    Args:
+        transform: 4x4 np array representing the transform we want to do on our point
+        point_as_array: 3x1 np array representing the location of a point in 3D
+
+    Returns:
+        transformed 3D point in homogenous coordinates
+    """
+
+    # convert 3D point to homogenous coordinates
     in_point = np.ones((4, 1))
     in_point[0][0] = point_as_array[0]
     in_point[1][0] = point_as_array[1]
     in_point[2][0] = point_as_array[2]
+
+    # multiply homogenous coord by transform
     out_point = np.matmul(transform, in_point)
     return out_point
 
 
+##### POINTER SPECIFIC UTILS FUNCTIONS
 def pointer_to_mri(Cam_T_PntRef, Cam_T_PatRef, PatRef_T_MRI, point_in_pointer_coords=[0, 0, 0]):
     """
     Converts a point in pointer space to MRI space.
+
+    Args:
+        Cam_T_PntRef: 4x4 transform from pointer's reference to camera coords
+        Cam_T_PatRef: 4x4 transform from patient's reference to camera coords
+        PatRef_T_MRI: 4x4 transform from MRI to patient reference coords
+        point_in_pointer_coords: 3x1/1x3 list representing 3D point we are transforming from pointer to MRI space
+
+    Returns:
+        point_in_pointer_coords but in MRI space
     """
+
     transform = np.linalg.inv(PatRef_T_MRI) @ np.linalg.inv(Cam_T_PatRef) @ Cam_T_PntRef
     out_point = multiply_point_by_matrix(transform, point_in_pointer_coords)
     return out_point
 
 
+##### ENDOSCOPE SPECIFIC UTILS FUNCTIONS
 def camera_to_mri(Cam_T_Endo, Cam_T_PatRef, PatRef_T_MRI, Eye_T_Hand, point_in_camera_coords=[0, 0, 0]):
+    """
+
+    Args:
+        Cam_T_Endo:
+        Cam_T_PatRef:
+        PatRef_T_MRI:
+        Eye_T_Hand:
+        point_in_camera_coords:
+
+    Returns:
+
+    """
     """
     Converts a point in camera space to MRI space.
     """
@@ -77,6 +117,18 @@ def camera_to_mri(Cam_T_Endo, Cam_T_PatRef, PatRef_T_MRI, Eye_T_Hand, point_in_c
 
 def mri_to_camera(Cam_T_Endo, Cam_T_PatRef, PatRef_T_MRI, Eye_T_Hand, point_in_mri_coords=[0, 0, 0]):
     """
+
+    Args:
+        Cam_T_Endo:
+        Cam_T_PatRef:
+        PatRef_T_MRI:
+        Eye_T_Hand:
+        point_in_mri_coords:
+
+    Returns:
+
+    """
+    """
     Converts a point in camera space to MRI space.
     """
     transform = np.linalg.inv(PatRef_T_MRI) @ np.linalg.inv(Cam_T_PatRef) @ Cam_T_Endo @ np.linalg.inv(Eye_T_Hand)
@@ -85,22 +137,19 @@ def mri_to_camera(Cam_T_Endo, Cam_T_PatRef, PatRef_T_MRI, Eye_T_Hand, point_in_m
     return out_point
 
 
-def add_noise_to_points(points_in, sigma):
-    points_out = np.zeros((points_in.shape))
-    for r in range(points_in.shape[0]):
-        for c in range(points_in.shape[1]):
-            points_out[r][c] = points_in[r][c] + random.normalvariate(0, sigma)
-    return points_out
-
-
-def add_noise_to_params(params, sigma):
-    params_out = copy.deepcopy(params)
-    for i, p in enumerate(params):
-        params_out[i] = params[i] + random.normalvariate(0, sigma)
-    return params_out
-
-
+#############
 def extract_rigid_body_parameters(matrix):
+    """
+    extract_rigid_body_parameters(matrix)
+    extracts parameters from transformation matrix
+
+    Args:
+        matrix: 4x4 transformation matrix
+
+    Returns:
+        list of all extracted parameters from matrix
+
+    """
     t = matrix[0:3, 3]
     r = matrix[0:3, 0:3]
     rot = spr.from_matrix(r)
@@ -109,6 +158,17 @@ def extract_rigid_body_parameters(matrix):
 
 
 def rigid_body_parameters_to_matrix(params):
+    """
+    rigid_body_parameters_to_matrix(params)
+    converts a list of rigid body parameters to transformation matrix
+
+    Args:
+        params: list of rigid body parameters
+
+    Returns:
+        4x4 transformation matrix of these parameters
+
+    """
     matrix = np.eye(4)
     r = (spr.from_euler('zyx', [params[0], params[1], params[2]], degrees=True)).as_matrix()
     matrix[0:3, 0:3] = r
@@ -127,10 +187,39 @@ def convert_4x1_to_1x1x3(p_41):
 
 
 def project_camera_point_to_image(point, intrinsics, distortion):
+    """
+    project_camera_point_to_image(point, intrinsics, distortion)
+    projects a 3D point into 2D space
+
+    Args:
+        point: 4x1 np array of homogeneous 3D point we are projecting
+        intrinsics: intrinsics matrix of camera
+        distortion: distortion coeffs of camera
+
+    Returns:
+        2D projected point
+    """
     rvec = np.zeros((1,3))
     tvec = np.zeros((1,3))
     image_points, jacobian = cv2.projectPoints(convert_4x1_to_1x1x3(point), rvec, tvec, intrinsics, distortion)
     return image_points[0][0] # returns a list
+
+
+def project_camera_points_to_image(points, intrinsics, distortion):
+    """
+    TO ADD
+    """
+    rvec = np.zeros((1,3))
+    tvec = np.zeros((1,3))
+    # convert_4x1_to_1x1x3(point)
+
+    #for idx in range(len(points)):
+    #    print(points[idx,:])
+    #    points[idx,:] = convert_4x1_to_1x1x3(np.array(points[idx,:]).T)
+
+    image_points, jacobian = cv2.projectPoints(points, rvec, tvec, intrinsics, distortion)
+
+    return image_points# returns a list
 
 
 def get_ref_T_tip(pointer_length, dimension):
@@ -155,7 +244,6 @@ def get_ref_T_tip(pointer_length, dimension):
 
 
 ####################### REFERENCE DATA
-
 def create_pnt_ref():
 
     # Creating pointer reference (from datasheet). Using homogenous (4 numbers, x,y,z,1) as row vectors.
@@ -268,11 +356,81 @@ def create_pat_ref_in_camera_space():
 
     return pat_ref_in_camera_space
 
+
+###### NOISE
+def add_noise_to_points(points_in, sigma):
+    """
+    add_noise_to_points(points_in, sigma)
+    adding noise to 3D points
+
+    Args:
+        points_in: 3xN matrix of points we are adding noise to
+        sigma: standard deviation of normal distribution defining noise distribution
+
+    Returns:
+        same points but with random noise added to them
+    """
+    points_out = np.zeros((points_in.shape))
+    for r in range(points_in.shape[0]):
+        for c in range(points_in.shape[1]):
+            points_out[r][c] = points_in[r][c] + random.normalvariate(0, sigma)
+    return points_out
+
+
+def add_noise_to_params(params, sigma):
+    """
+    add_noise_to_params(params, sigma)
+    adds noise to parameters (translation, rotation etc)
+
+    Args:
+        params: list of all parameters (translations in xyz, rotations in etc.)
+        sigma: standard deviation of normal noise to add
+
+    Returns:
+        parameters with noise added on them
+    """
+    params_out = copy.deepcopy(params)
+    for i, p in enumerate(params):
+        params_out[i] = params[i] + random.normalvariate(0, sigma)
+    return params_out
+
+
 def calculate_euclid_dist(pointer_tip_in_mri_space,tumour_in_mri_space ):
+    """
+    calculate_euclid_dist(pointer_tip_in_mri_space,tumour_in_mri_space )
+    calculates euclidean distance between two 3D points (euclid_dist is x^2+y^2+x^2)
+
+    Args:
+        pointer_tip_in_mri_space: 4x1 np array of where we think the tumor is
+        tumour_in_mri_space: 4x1 np array of where the tumor actually is
+
+    Returns:
+        euclidean distance between the two points
+    """
+
     dist =  (pointer_tip_in_mri_space[0] - tumour_in_mri_space[0]) \
-           * (pointer_tip_in_mri_space[0] - tumour_in_mri_space[0]) \
-           + (pointer_tip_in_mri_space[1] - tumour_in_mri_space[1]) \
-           * (pointer_tip_in_mri_space[1] - tumour_in_mri_space[1]) \
-           + (pointer_tip_in_mri_space[2] - tumour_in_mri_space[2]) \
-           * (pointer_tip_in_mri_space[2] - tumour_in_mri_space[2])
+            * (pointer_tip_in_mri_space[0] - tumour_in_mri_space[0]) \
+            + (pointer_tip_in_mri_space[1] - tumour_in_mri_space[1]) \
+            * (pointer_tip_in_mri_space[1] - tumour_in_mri_space[1]) \
+            + (pointer_tip_in_mri_space[2] - tumour_in_mri_space[2]) \
+            * (pointer_tip_in_mri_space[2] - tumour_in_mri_space[2])
+
     return dist
+
+
+###### converting shapes
+def convert_points_1x3_to_1x4x3(p_13):
+    p_113 = np.zeros((1,1,3))
+    p_113[0][0][0] = p_13[0]
+    p_113[0][0][1] = p_13[1]
+    p_113[0][0][2] = p_13[2]
+    return p_113
+
+
+def convert_points_nx3_to_1xnx3(pn3):
+    n = len(pn3)
+    p_1n3 = np.zeros((1,n,3))
+
+    for idx, point in enumerate(pn3):
+        p_1n3[:,idx ,:] = convert_points_1x3_to_1x4x3(point)
+    return p_1n3
